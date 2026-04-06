0369c1a186a0bf63b285c5e33c555595c14af952
zavemaan-droid <zavemaan@gmail.com>
2026-04-06 07:04:05 -0700

    Updated package-lock.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
-import { Stack } from 'expo-router'
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+
 export default function RootLayout() {
   useFrameworkReady();
-  return <Stack screenOptions={{ headerShown: false }} />;
+
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
+  );
 }
